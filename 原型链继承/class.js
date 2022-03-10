07940147d738cc6c59667a582ad3fd8f780e2aae
DanielLin0516 <daniellin0516@gmail.com>
2022-03-10 23:04:55 +0800

    继承

--- 原型链继承/class.js
+++ 原型链继承/class.js
@@ -11,4 +11,17 @@ class Son extends Father {
     }
 }
 let son = new Son('red','red');
-console.log(son.name)
+// 原型链继承
+function Dad(colors = ['red', 'blue', 'green']) {
+    this.colors = colors;
+}
+function Sun() {
+
+}
+Sun.prototype = new Dad();
+let ins = new Dad(["red"])
+let sun = new Sun();
+let sun1= new Sun();
+console.log(sun.colors)
+sun.colors.push("oo");
+console.log(sun.colors,sun1.colors,ins.colors)
