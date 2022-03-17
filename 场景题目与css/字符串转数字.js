77a7a5b708d96b18a5ceeb25ea0a8858db2e7163
DanielLin0516 <daniellin0516@gmail.com>
2022-03-17 23:11:21 +0800

    3.17

---
+++ 场景题目与css/字符串转数字.js
@@ -0,0 +1,16 @@
+
+let str = "[abc[bcd[def]]]"
+
+function normallize() {
+    let arr = str.split(/[\[\]]/g).filter(Boolean);
+    let result = {};
+    arr.reduce((pre,cur,index,self) => {
+        pre.value = cur;
+        if(index !== self.length - 1) {
+            pre.children = {};
+            return pre.children;
+        }
+    },result)
+    return result;
+}
+console.log(normallize(str))
