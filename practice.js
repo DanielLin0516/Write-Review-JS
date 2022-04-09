4018945fb1ab85b8e7494cd4544675949a4ee360
DanielLin0516 <daniellin0516@gmail.com>
2022-04-09 23:22:07 +0800

    4.9

--- practice.js
+++ practice.js
@@ -45,3 +45,20 @@ function cuur2(fn) {
         return fn(...ars)
     }
 }
+
+function deepClone(target,map = new Map()) {
+    if(typeof target === 'object' && target !== null) {
+        let cache = map.get(target);
+        if(cache) return cache
+        let result = Array.isArray(target) ? [] : {};
+        map.set(target,result)
+        for(let item in target) {
+            if(target.hasOwnProperty(item)) {
+                result[item] = deepClone(target[item]);
+            }
+        }
+        return result;
+    }else {
+        return target;
+    }
+}
--- 原型链继承/寄生式组合继承.js
+++ 原型链继承/寄生式组合继承.js
@@ -1,7 +1,7 @@
 function inheritPrototype(subType, superType) {
-    var prototype = Object.create(superType.prototype); // 创建对象，创建父类原型的一个副本
-    prototype.constructor = subType;                    // 增强对象，弥补因重写原型而失去的默认的constructor 属性
-    subType.prototype = prototype;                      // 指定对象，将新创建的对象赋值给子类的原型
+    let prototype = Object.create(superType.prototype); // 创建对象，创建父类原型的一个副本
+    prototype.constructor = subType;                   // 增强对象，弥补因重写原型而失去的默认的constructor 属性
+    subType.prototype = prototype;                     // 指定对象，将新创建的对象赋值给子类的原型
 }
 
 // 父类初始化实例属性和原型属性
@@ -10,7 +10,7 @@ function SuperType(name) {
     this.colors = ["red", "blue", "green"];
 }
 SuperType.prototype.sayName = function () {
-    alert(this.name);
+    console.log(this.name);
 };
 
 // 借用构造函数传递增强子类实例属性（支持传参和避免篡改）
@@ -23,7 +23,8 @@ function SubType(name, age) {
 inheritPrototype(SubType, SuperType);
 
 // 新增子类原型属性
-SubType.prototype.sayAge = function () {z
+SubType.prototype.sayAge = function () {
+    z
     alert(this.age);
 }
 
@@ -32,4 +33,5 @@ var instance2 = new SubType("lxy", 23);
 
 instance1.colors.push("2"); // ["red", "blue", "green", "2"]
 instance1.colors.push("3"); // ["red", "blue", "green", "3"]
+instance1.sayName()
 
