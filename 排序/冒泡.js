07a084681f27898385dd13fc56822bef97f0f9ca
DanielLin <daniellin0516@gmail.com>
2022-11-13 17:27:08 +0800

    fix: 冒泡排序

--- 排序/冒泡.js
+++ 排序/冒泡.js
@@ -1,9 +1,9 @@
 const arr = [5, 2, 7, 8, 34, 7, 39, 12, 56, 9, 1]
 function bubbleSort(arr) {
-    for (let j = 0; j < arr.length; j++) {
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                swap(arr, i, i + 1)
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1)
             }
         }
     }
